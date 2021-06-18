203039ac92cbe353ac8963a47b03dc542a57be4b
phobo99 <phobotkcb@gmail.com>
2021-06-18 23:36:29 +0700

    First Project Basic

--- Instance/main.js
+++ Instance/main.js
@@ -1,11 +1,16 @@
 var vueInstance = new Vue({
     el: '#app',
     data: {
-        title: 'Dien thoai Samsung'
+        title: 'Dien thoai Samsung',
+        value: ''
     },
     methods: {
         say: function(text) {
             return 'Hello '+ text + ' this is Methods'
+        },
+        // thay đổi value theo realtime khi ngườI nhập vào input
+        changeValue: function(event) {
+            this.value = event.target.value;
         }
     }
 })
